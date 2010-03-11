c9ffaf36872d2f90c79a752878f9f7a1cc3a7a00
Jay Hannah <jay@jays.net>
2010-03-11 16:06:16 -0600

    I broke a long one-liner in matching.html into it's own perl program (dist.pl) and changed it in minor ways to get it working.

--- dist.pl
+++ dist.pl
@@ -3,7 +3,9 @@
 # This program is listed as a one-liner in matching.html. One-liners are hard to debug,
 # though, so I'm pulling it out into a normal program.  --jhannah 20100311
 
-$dist = 1000;
+use strict;
+
+my (@data, @olddata, $dist);
 while (<>) {
    @data = split; 
    if ($data[3] eq "-") {
@@ -13,13 +15,13 @@ while (<>) {
          }
          print "@data[0..5] 1000 $data[6]\n"
       } else {
-         $dist = $data[2] - $olddata[2]; 
          print "@olddata[0..5] $dist $data[6]\n@data[0..5] $dist $data[6]\n";
       } 
+   } else {
+      $dist = @olddata ? $data[2] - $olddata[2] : 1000;
    }
    @olddata = @data;
 }
-print "@olddata[0..5] 1000 $data[6]\n";ii
-
+print "@olddata[0..5] 1000 $data[6]\n";
 
 
